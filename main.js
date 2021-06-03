fea68f15b038f3593d68518d7b57c48565514a80
Alex Kozlov <Lelikk86@gmail.com>
2021-06-03 08:58:13 +0800

    правки после homeWork-2

--- main.js
+++ main.js
@@ -1,26 +1,53 @@
 const subZero = {
-  name: "Sub-Zero",
-  hp: 80,
-  img: "http://reactmarathon-api.herokuapp.com/assets/subzero.gif",
-  weapon: ["ice", "cream"],
-  attack: function () {
-    console.log(this.name + " Fight...");
+    name: "Sub-Zero",
+    hp: 100,
+    img: "http://reactmarathon-api.herokuapp.com/assets/subzero.gif",
+    weapon: ["ice", "cream"],
+    attack: function () {
+      console.log(this.name + " Fight...");
+    },
   },
-};
+  kitana = {
+    name: "Kitana",
+    hp: 90,
+    img: "http://reactmarathon-api.herokuapp.com/assets/kitana.gif",
+    weapon: ["nuclear", "mouth"],
+    attack: function () {
+      console.log(this.name + " Fight...");
+    },
+  },
+  scorpion = {
+    name: "Scorpion",
+    hp: 80,
+    img: "http://reactmarathon-api.herokuapp.com/assets/scorpion.gif",
+    weapon: ["nuclear", "mouth"],
+    attack: function () {
+      console.log(this.name + " Fight...");
+    },
+  },
+  liukang = {
+    name: "Liu-Kang",
+    hp: 70,
+    img: "http://reactmarathon-api.herokuapp.com/assets/liukang.gif",
+    weapon: ["nuclear", "mouth"],
+    attack: function () {
+      console.log(this.name + " Fight...");
+    },
+  },
+  sonya = {
+    name: "Sonya",
+    hp: 60,
+    img: "http://reactmarathon-api.herokuapp.com/assets/sonya.gif",
+    weapon: ["nuclear", "mouth"],
+    attack: function () {
+      console.log(this.name + " Fight...");
+    },
+  };
 
-const kitana = {
-  name: "Kitana",
-  hp: 60,
-  img: "http://reactmarathon-api.herokuapp.com/assets/kitana.gif",
-  weapon: ["nuclear", "mouth"],
-  attack: function () {
-    console.log(this.name + " Fight...");
-  },
-};
+const $arenas = document.querySelector(".arenas");
 
 function createPlayer(player, character) {
-  const $arenas = document.querySelector(".arenas"),
-    $player = document.createElement("div"),
+  const $player = document.createElement("div"),
     $progressbar = document.createElement("div"),
     $character = document.createElement("div"),
     $life = document.createElement("div"),
@@ -45,5 +72,5 @@ function createPlayer(player, character) {
   $name.innerText = character.name;
 }
 
-createPlayer("player1", subZero);
-createPlayer("player2", kitana);
+createPlayer("player1", liukang);
+createPlayer("player2", scorpion);
